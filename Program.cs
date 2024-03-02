dd9e55b8fed88740a5fce21aef54c6e0ac513641
Regiane_de_Alcântara <any_any_2011@hotmail.com>
2024-03-02 19:32:21 -0300

    InicioProjeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+// Fazer um código para que o usuario adivinhe
+// Uma palavra secreta String
+// terá 5 tentativas psrs adivinhar a palavra secreta
+// aparecer em tela os campos 
+// Digite a palavra secreta
+// Digite palpite
+//Informar se acertou a palavra secreta 
+//Infomar se errou a palavra secreta, tentar novamente
+//Se errar todas as 5 tentativas, informar que acabaram as tentativas 
+// Informar qual era a palavra secreta
+  
